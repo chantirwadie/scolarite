0552a1262abb71bcf3359636f018c0926a49329d
chantirwadie <chantirwadie@gmail.com>
2022-05-19 19:23:15 +0100

    Remove a hardcoded Django secret key

--- scolarite/settings.py
+++ scolarite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)zlcgdjuod5lq5#2k1$9g=o7c%0gat44(wob!g_lj-2!i0+z9'
+#SECRET_KEY = 'django-insecure-3)zlcgdjuod5lq5#2k1$9g=o7c%0gat44(wob!g_lj-2!i0+z9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
